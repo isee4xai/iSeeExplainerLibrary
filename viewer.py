625ea0591e1eeef2e73ffbc0b4f33d5a4114b95e
Jesus Darias <jdarias@ucm.es>
2023-04-13 12:28:28 +0200

    Add NLP comparison for explainers

--- viewer.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Resource
-from werkzeug.utils import secure_filename
-
-class ViewExplanation(Resource):
-    def get(self, filename):
-        return {  
-            "filename": secure_filename(filename),
-        }
